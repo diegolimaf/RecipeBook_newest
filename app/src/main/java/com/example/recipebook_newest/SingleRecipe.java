44be0fb8775a34f6ea4ccc152fe4406edfce6c51
diegolimaf <diegolimaf3@hotmail.com>
2020-02-12 16:34:10 -0500

    The recycler view it is done. I've created the activity for last page.

---
+++ app/src/main/java/com/example/recipebook_newest/SingleRecipe.java
@@ -0,0 +1,14 @@
+package com.example.recipebook_newest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_recipe);
+    }
+}
